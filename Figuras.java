d91a44bd4fd737fc82d5f44918048c9f8e030441
Daniel Sánchez <95046702+alusan9143@users.noreply.github.com>
2022-11-08 20:05:28 +0100

    Create Figuras.java

---
+++ Figuras.java
@@ -0,0 +1,18 @@
+public class Rectangulo {
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo (double lado1, double lado2) {
+        lado1 = l1;
+        lado2 = l2;
+    }
+
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+
+    public double obtenerPerimetro(){
+        return (lado1*2)+(lado2*2);
+    }
+
+}
